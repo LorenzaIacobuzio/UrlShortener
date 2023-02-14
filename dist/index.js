f31b8fd4f445ae08063da34a0f720cef9da873cd
LorenzaIacobuzio <lorenza.iacobuzio@gmail.com>
2023-02-14 15:53:32 +0100

    Add shorten endpoint - preliminary version

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,26 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 8080;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
-});
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+app.get('/', (req, res) => {
+    res.status(200).send("All is good");
+});
+app.post('/shorten', (req, res) => {
+    let reqUrl = req.body.url;
+    if (reqUrl === undefined) {
+        res.status(400).send("Url undefined");
+    }
+    else {
+        try {
+            res.status(200).send({ url: reqUrl + '/1' });
+        }
+        catch (error) {
+            res.status(500).send("Internal server error");
+        }
+    }
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = 8080;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+const app: Express = express()
+const port = 8080
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send("All is good")
+})
+
+app.post('/shorten', (req: Request, res: Response) => {
+    let reqUrl = req.body.url
+    if (reqUrl === undefined) {
+        res.status(400).send("400: url undefined")
+    } else {
+        try {
+            res.status(200).send({url: reqUrl + '/1'})
+        } catch (error) {
+            res.status(500).send("500: internal server error")
+        }
+    }
+})
